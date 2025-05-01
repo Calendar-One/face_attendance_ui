3f531df2b40cda2649931ad83464bcdae224ef80
qianhao <qianhao_yin@calendarone.com>
2025-05-01 23:36:13 +0800

    add ToastContainer

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import AppRouter from './router.tsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
+    <ToastContainer />
   </StrictMode>,
 )
