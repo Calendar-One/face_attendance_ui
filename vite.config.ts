afe17f5be6a39cb12464a5ea1e20899496124db4
qianhao <qianhao_yin@calendarone.com>
2025-05-01 16:21:43 +0800

    chore: fix the 404 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/face_attendance_ui/",
+  base: "/face_attendance_ui",
 });
